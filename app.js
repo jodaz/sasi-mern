5eca8d544f9d143170aaf7cb025a4208f7c3f858
andreina santana <andreinajsantanag@gmail.com>
2020-05-30 09:00:55 -0430

    Fix path

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const passport = require('passport');
+const path = require('path');
 const app = express();
 
 // Setting up
